e8b5367631ad7e0194ef8589e44a90ef93164dfd
Signe Kjær Brandt Hau <signehau@gmail.com>
2026-05-14 19:13:20 +0200

    Opdatering af billede navne

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -18,7 +18,7 @@ export default function Hero() {
     <section className="relative w-full h-96 md:h-175 overflow-visible z-20">
       
       <Image
-        src="/img/hero1.png"
+        src="/img/intro-img.png"
         alt="Ukraine indsats intro"
         fill
         className="object-cover"
